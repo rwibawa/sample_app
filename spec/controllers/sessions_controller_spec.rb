82d760502a9d02213532efcf8e2e40c6f550ce28
Ryan Wibawa <ryan.wibawa@gmail.com>
2011-07-24 17:00:49 -0700

    Done with user edit/update, index, and destroy actions

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -61,12 +61,14 @@ describe SessionsController do
   end
   
   describe "DELETE 'destroy'" do
+  
     it "should sing a user out" do
       test_sign_in(Factory(:user))
       delete :destroy
       controller.should_not be_signed_in
       response.should redirect_to(root_path)
     end
+    
   end
 
 end
